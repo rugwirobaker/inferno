401f99682434c4ad7af3e61bb50484e9a6d4b034
Rugwiro Mbanda Valentin <rugwirobaker@gmail.com>
2024-10-20 00:50:06 +0000

    Add a vsock client for the host call init control API

--- internal/vsock/vsock.go
+++ internal/vsock/vsock.go
@@ -1,11 +1,15 @@
 package vsock
 
 import (
+	"bufio"
 	"context"
 	"fmt"
+	"log/slog"
 	"net"
 	"net/http"
 	"os"
+	"path/filepath"
+	"time"
 
 	"github.com/mdlayher/vsock"
 )
@@ -39,6 +43,46 @@ func NewClient(ctx context.Context, port uint32) (*http.Client, error) {
 	return client, nil
 }
 
+// NewGuestClient creates a new http client that connects that the host uses to connect to the guest
+// it will mainly be used to call the unit control API
+func NewGuestClient(chroot string, port int) *http.Client {
+	vsockPath := filepath.Join(chroot, "control.sock")
+
+	return &http.Client{
+		Timeout: 5 * time.Second,
+		Transport: &maxBytesTransport{
+			Transport: &http.Transport{
+				DisableKeepAlives: true,
+				DialContext: func(_ context.Context, _, _ string) (net.Conn, error) {
+					conn, err := net.DialTimeout("unix", vsockPath, 1*time.Second)
+					if err != nil {
+						return nil, fmt.Errorf("could not dial %s: %w", vsockPath, err)
+					}
+
+					n, err := conn.Write([]byte(fmt.Sprintf("CONNECT %d\n", port)))
+					if err != nil {
+						return nil, fmt.Errorf("could not write vsock CONNECT %d line: %w", port, err)
+					}
+					slog.Debug("wrote", "bytes", n)
+
+					r := bufio.NewReader(conn)
+
+					// read one line (OK 123456789)
+					slog.Debug("reading OK line from vsock")
+					l, _, err := r.ReadLine()
+					if err != nil {
+						return nil, fmt.Errorf("could not read OK line from vsock: %w", err)
+					}
+
+					slog.Debug("connection established", "read", string(l))
+
+					return conn, nil
+				},
+			},
+		},
+	}
+}
+
 // NewVsockListener creates a new vsock listener on the specified port.
 // Its used by the guest to communicate with the host via /dev/vsock
 func NewVsockListener(port uint32) (net.Listener, error) {
@@ -63,4 +107,21 @@ func NewVsockUnixListener(path string) (net.Listener, error) {
 		return nil, fmt.Errorf("failed to start vsock listener: %w", err)
 	}
 	return listener, nil
+}
+
+type maxBytesTransport struct {
+	Transport   http.RoundTripper
+	MaxBodySize int64
+}
+
+func (t *maxBytesTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	resp, err := t.Transport.RoundTrip(req)
+	if err != nil {
+		return nil, err
+	}
+
+	// Limit the response body to the specified maximum size
+	resp.Body = http.MaxBytesReader(nil, resp.Body, t.MaxBodySize)
+
+	return resp, nil
 }
