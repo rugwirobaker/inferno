3cdf6b4815ef485fdc2b6af0eff1ffe5b717ea9b
Rugwiro Mbanda Valentin <rugwirobaker@gmail.com>
2024-10-22 23:38:47 +0000

    Add some useful scripts

---
+++ scripts/kernel.sh
@@ -0,0 +1,9 @@
+ARCH="$(uname -m)"
+
+latest=$(wget "http://spec.ccfc.min.s3.amazonaws.com/?prefix=firecracker-ci/v1.10/x86_64/vmlinux-5.10&list-type=2" -O - 2>/dev/null | grep "(?<=<Key>)(firecracker-ci/v1.10/x86_64/vmlinux-5\.10\.[0-9]{3})(?=</Key>)" -o -P)
+
+# Download a linux kernel binary
+wget "https://s3.amazonaws.com/spec.ccfc.min/${latest}"
+
+# Rename the binary
+mv "vmlinux-5.10.${latest}" "vmlinux"
